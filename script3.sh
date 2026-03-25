c26de4cdb2fde71778fe3c267f100a3b6041fc89
anushkaa-star <anushka.24bai10805@vitbhopal.ac.in>
2026-03-26 00:35:51 +0530

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
-DIRS=("/etc" "/home" "/usr" "/tmp")
+# Script 3: Disk and Permission Auditor
+# This script checks directory size and permissions
+
+# List of directories
+DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
 
 echo "Directory Audit Report"
 echo "----------------------"
 
+# Loop through directories
 for DIR in "${DIRS[@]}"
 do
     if [ -d "$DIR" ]
     then
-        PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
+        PERM=$(ls -ld $DIR | awk '{print $1, $3, $4}')
         SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
-        echo "$DIR => Permissions: $PERMS | Size: $SIZE"
+        echo "$DIR => Permissions: $PERM | Size: $SIZE"
     else
-        echo "$DIR does not exist"
+        echo "$DIR does not exist on this system"
     fi
 done
+
+# Check Python config directory
+CONFIG_DIR="/etc/python3"
+
+if [ -d "$CONFIG_DIR" ]; then
+    PERM=$(ls -ld $CONFIG_DIR | awk '{print $1, $3, $4}')
+    echo "Config Dir ($CONFIG_DIR) => Permissions: $PERM"
+else
+    echo "Config directory not found"
+fi
